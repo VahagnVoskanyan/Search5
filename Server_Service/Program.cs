0e54e15a5ea8e6d1432cb11ebd7f206bb2a11e59
Vahag <vahagnlvoskanyan@gmail.com>
2025-09-25 17:05:15 +0400

    ...

--- Server_Service/Program.cs
+++ Server_Service/Program.cs
@@ -4,82 +4,83 @@ using Server_Service.Data;
 using Server_Service.SyncDataServices.gRPC;
 using Server_Service.EventProcessing;
 
-namespace Server_Service;
-
-public class Program
+namespace Server_Service
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        // Uncomment for local testing real SQL Server
-        /*Console.WriteLine("--> Using SqlServer Db");
-        builder.Services.AddDbContext<AppDbContext>(opt =>
-            opt.UseSqlServer(builder.Configuration.GetConnectionString("CustomersConn")));*/
-
-        // Comment for local testing real SQL Server
-        if (builder.Environment.IsProduction()) // in K8S
+        public static void Main(string[] args)
         {
-            Console.WriteLine("--> Using SqlServer Db");
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Uncomment for local testing real SQL Server
+            /*Console.WriteLine("--> Using SqlServer Db");
             builder.Services.AddDbContext<AppDbContext>(opt =>
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("CustomersConn")));
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("CustomersConn")));*/
+
+            // Comment for local testing real SQL Server
+            if (builder.Environment.IsProduction()) // in K8S
+            {
+                Console.WriteLine("--> Using SqlServer Db");
+                builder.Services.AddDbContext<AppDbContext>(opt =>
+                    opt.UseSqlServer(builder.Configuration.GetConnectionString("CustomersConn")));
+            }
+            else
+            {
+                Console.WriteLine("--> Using InMem Db");
+                builder.Services.AddDbContext<AppDbContext>(opt =>
+                    opt.UseInMemoryDatabase("InMem"));
+            }
+
+
+            builder.Services.AddScoped<ICustomerRepo, CustomerRepo>();
+
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); //For Dtos
+            builder.Services.AddSingleton<IEventProcessor, EventProcessor>(); // Singleton => for Message Bus
+            builder.Services.AddHostedService<MessageBusSubscriber>();                //Subscribe from bus
+            builder.Services.AddGrpc(); // gRPC
+
+
+            var app = builder.Build();
+
+            /*try
+            {
+                using var scope = app.Services.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                app.Logger.LogError("Automatic Migration Error: {ex}", ex);
+            }*/
+
+            PrepDb.PrepPopulation(app); //For Mock data
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            
+            app.UseHttpsRedirection();
+
+            //app.UseRouting(); // 
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.MapGrpcService<GrpcCustomerService>(); // gRPC
+
+            app.MapGet("/protos/customers.proto", async context =>
+            {
+                await context.Response.WriteAsync(File.ReadAllText("Protos/customers.proto"));
+            }); //It's not not necessary, we give info about proto file
+
+            app.Run();
         }
-        else
-        {
-            Console.WriteLine("--> Using InMem Db");
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-                opt.UseInMemoryDatabase("InMem"));
-        }
-
-
-        builder.Services.AddScoped<ICustomerRepo, CustomerRepo>();
-
-        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); //For Dtos
-                                                                                 //builder.Services.AddSingleton<IEventProcessor, EventProcessor>(); // Singleton => for Message Bus
-                                                                                 //builder.Services.AddHostedService<MessageBusSubscriber>();                //Subscribe from bus
-                                                                                 //builder.Services.AddGrpc(); // gRPC
-
-
-        var app = builder.Build();
-
-        /*try
-        {
-            using var scope = app.Services.CreateScope();
-            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            db.Database.Migrate();
-        }
-        catch (Exception ex)
-        {
-            app.Logger.LogError("Automatic Migration Error: {ex}", ex);
-        }*/
-
-        PrepDb.PrepPopulation(app); //For Mock data
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        //app.UseRouting(); // 
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        //app.MapGrpcService<GrpcCustomerService>(); // gRPC
-
-        app.MapGet("/protos/customers.proto", async context =>
-        {
-            await context.Response.WriteAsync(File.ReadAllText("Protos/customers.proto"));
-        }); //It's not not necessary, we give info about proto file
-
-        app.Run();
     }
 }
